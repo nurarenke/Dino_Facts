2a92304f097243ddcf25c6da6a2128c348df1547
Nura Renke <nurarenke@gmail.com>
2018-01-25 15:50:37 -0800

    onclick function

--- src/index.js
+++ src/index.js
@@ -2,34 +2,61 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Button extends React.Component {
+function Button(props){
+
+    return (
+        <button className="randomFactButton" onClick={props.onClick}>
+        {props.btnText}
+        </button>
+    );
+    
+} 
+
+const dinoFacts = ["Dinosaurs are a group of reptiles that have lived on Earth for about 245 million years.",
+"At present over 700 different species of dinosaurs have been identified and named."]
+
+const Result = (props) => {
+    return (
+        <div>{props.dinoFact}</div>
+    );
+};
+
+class App extends React.Component {
     constructor(props){
         super(props);
-        this.state={btnText: "Click to get started"}
-    }
-
-    handleClick = () => {
-        this.setState({
-            btnText: "New Fact"
-        })
-
-    };
-    render() {
-        return (
-            <button className="randomFactButton" onClick={this.handleClick}>
-            {this.state.btnText}
-            </button>
-        );
+        this.state={
+            btnText:"Click to get started",
+            dinoFact:"Let's learn about dinos!"}
     }
     
+
+    changeBtnText = () => {
+        let currentDinoFact = findNewFact(dinoFacts);
+        this.setState((prevState) => ({
+            btnText: "New Fact",
+            dinoFact: currentDinoFact
+        }));
+    };
+
+    render() {
+        
+        return(
+            <div>
+                <Result dinoFact={this.state.dinoFact} />
+                <Button onClick={this.changeBtnText} btnText={this.state.btnText} />
+            </div>
+        );
+    }
 }
-
-
 
 
 // ========================================
 
 ReactDOM.render(
-  <Button />,
+  <App />,
   document.getElementById('root')
 );
+
+function findNewFact(dinoFacts){
+    return dinoFacts[Math.floor(Math.random()*dinoFacts.length)];
+}
