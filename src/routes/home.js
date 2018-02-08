8d210b9eec79d4b71a9db4b2dbd1a9c337b66c28
Nura Renke <nurarenke@gmail.com>
2018-02-07 16:19:38 -0800

    pushed router history

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 
+const showLocation = withRouter(Button);
 
 function Button(props){
     return (
-        <Link to={{ pathname:'/home/' + props.factIndex}}>
-            <button 
-                className="randomFactButton btn btn-primary button" 
-                onClick={props.onClick}>
-                {props.btnText}
-            </button>
-        </Link>
+        <button 
+            className="randomFactButton btn btn-primary button" 
+            onClick={props.onClick}>
+            {props.btnText}
+        </button>
     );
-    
-} 
+}
 
 const dinoFacts = ["Dinosaurs are a group of reptiles that have lived on Earth for about 245 million years.",
 "At present over 700 different species of dinosaurs have been identified and named."]
@@ -37,17 +35,21 @@ export default class Home extends React.Component {
 
     onNewFactClicked = () => {
         let newDinoFactIndex = findNewFactIndex(dinoFacts)
+
         this.setState((prevState) => ({
             currentDinoFact: dinoFacts[newDinoFactIndex],
             factIndex: newDinoFactIndex
         }));
+
+        this.props.history.push('/home/' + this.state.factIndex)
+
     };
 
     render() {
         return(
             <div>
                 <Result dinoFact={this.state.currentDinoFact} />
-                <Button onClick={this.onNewFactClicked} btnText={this.state.btnText} factIndex={this.state.factIndex} />
+                <Button onClick={this.onNewFactClicked} btnText={this.state.btnText} factIndex={this.state.factIndex}/>
             </div>
         );
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch,} from 'react-router-dom';
 import Home from './home';
 
-export default () => (
+export default() => (
     <BrowserRouter>
         <Switch>
-            <Route path="/home" exact component={Home} />
+            <Route path="/" exact component={Home} />
             <Route exact path="/home/:id" exact render={props => <Home {...props} />}/>
         </Switch>
     </BrowserRouter>
